2d3e1555c1c40e62b964a5c9618fb362539a0155
Arun <arunprasad.rn@hotmail.com>
2018-02-20 11:20:39 +0000

    initial commit

---
+++ AzureAppServiceWebAppContainers/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AzureAppServiceWebAppContainers.Models;
+
+namespace AzureAppServiceWebAppContainers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Application running on Web App for  Containers";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            ViewData["Tittle"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
